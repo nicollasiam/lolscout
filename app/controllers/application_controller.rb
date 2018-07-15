b5b625d6065a80986f9891c52886670cbaa90322
Nicollas Isabel <nicollasisabel@hotmail.com>
2018-07-15 11:23:55 -0300

    Configured Pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,23 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+
   protect_from_forgery with: :exception
 
   # Devise
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = 'Você não possui autorização para fazer esta ação.'
+    redirect_to(request.referrer || root_path)
   end
 end
